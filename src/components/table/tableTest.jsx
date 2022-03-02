0a01a3d9b7740f7908448341fa17f29d5d530bed
jm61 <jean.moirano@gmail.com>
2022-03-02 17:30:45 +0100

    display columns in table

--- src/components/table/tableTest.jsx
+++ src/components/table/tableTest.jsx
@@ -8,8 +8,29 @@ const TableTest = () => {
   const columns = useMemo(() => Columns, []);
   const data = useMemo(() => fromLS, []);
   const tableInstance = useTable({columns,data})
+  const {
+    getTableProps,
+    headerGroups,
+  } = tableInstance
   return (
-    <h1>TABLE</h1>
+    <div className="table__container">
+      <header className="table__header">
+        <small>Header</small>    
+      </header>
+      <table className="table__contain" {...getTableProps()}>
+        <thead className="table__head">
+        {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup} className="table__head--tr">
+              {headerGroup.headers.map((column) => (
+                <th {...column} className="table__head--th">
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+      </table>
+    </div>
   )
 }
 export default TableTest
