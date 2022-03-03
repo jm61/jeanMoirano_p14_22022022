ce429cd10f9bc59e843df1468a1ca6659b8960f9
jm61 <jean.moirano@gmail.com>
2022-03-03 14:27:52 +0100

    added lazy/suspense from react, displays table rows

--- src/components/table/tableTest.jsx
+++ src/components/table/tableTest.jsx
@@ -1,5 +1,5 @@
 import './tableTest.css'
-import {useTable} from 'react-table'
+import {useTable, usePagination} from 'react-table'
 import {useMemo} from 'react'
 import {Columns} from './Columns'
 
@@ -7,10 +7,13 @@ const TableTest = () => {
   const fromLS = JSON.parse(localStorage.getItem("employees")) || []
   const columns = useMemo(() => Columns, []);
   const data = useMemo(() => fromLS, []);
-  const tableInstance = useTable({columns,data})
+  const tableInstance = useTable({columns,data}, usePagination)
   const {
     getTableProps,
     headerGroups,
+    prepareRow,
+    getTableBodyProps,
+    page
   } = tableInstance
   return (
     <div className="table__container">
@@ -18,6 +21,7 @@ const TableTest = () => {
         <small>Header</small>    
       </header>
       <table className="table__contain" {...getTableProps()}>
+        {/* Table Header */}
         <thead className="table__head">
         {headerGroups.map((headerGroup) => (
             <tr {...headerGroup} className="table__head--tr">
@@ -29,6 +33,25 @@ const TableTest = () => {
             </tr>
           ))}
         </thead>
+        {/* End of Table Header */}
+        {/* Table Body */}
+        <tbody {...getTableBodyProps()} className="table__body">
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()} className="table__body--tr">
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()} className="table__body--td">
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        {/* End of Table Body */}
       </table>
     </div>
   )
